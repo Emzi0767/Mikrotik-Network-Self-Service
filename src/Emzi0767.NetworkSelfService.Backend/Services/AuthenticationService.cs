784d19fa473f8d525d984d83a436eab424e027a2
Mateusz Brawański <git@emzi0767.dev>
2025-01-12 20:13:50 +0100

    Start working on the gRPC side.

---
+++ src/Emzi0767.NetworkSelfService.Backend/Services/AuthenticationService.cs
@@ -0,0 +1,45 @@
+﻿// This file is part of Network Self-Service Project.
+// Copyright © 2024-2025 Mateusz Brawański <Emzi0767>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using Emzi0767.NetworkSelfService.gRPC;
+using Grpc.Core;
+
+namespace Emzi0767.NetworkSelfService.Backend.Services;
+
+public sealed class AuthenticationService : Authentication.AuthenticationBase
+{
+    private readonly ILogger<AuthenticationService> _logger;
+
+    public AuthenticationService(ILogger<AuthenticationService> logger)
+    {
+        this._logger = logger;
+    }
+
+    public override Task<Result> Authenticate(AuthenticationRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new Result { IsSuccess = false, Error = new() { Code = ErrorCode.Unknown } });
+    }
+
+    public override Task<Result> RefreshSession(SessionRefreshRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new Result { IsSuccess = false, Error = new() { Code = ErrorCode.Unknown } });
+    }
+
+    public override Task<Result> DestroySession(SessionDestroyRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new Result { IsSuccess = false, Error = new() { Code = ErrorCode.Unknown } });
+    }
+}
